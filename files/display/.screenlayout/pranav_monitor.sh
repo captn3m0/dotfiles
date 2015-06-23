ce93d948ac700e4570f963ad230ddde2f3e3595d
Abhay Rana <capt.n3m0@gmail.com>
2015-06-24 01:43:38 +0530

    Adds second monitor arandr script

---
+++ files/display/.screenlayout/pranav_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x1152 --rotate normal --output DP1 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
