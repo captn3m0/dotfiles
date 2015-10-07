d06f0f88f8400944f5a50dd99f8f99dd44edca19
Abhay Rana <capt.n3m0@gmail.com>
2015-10-07 17:40:40 +0530

    Workarounds for DP1 monitor getting suspended

--- files/display/.screenlayout/work.sh
+++ files/display/.screenlayout/work.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1600x360 --rotate normal --output DP1 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
+xrandr --newmode  "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
+xrandr --addmode DP1 1600x900_60.00
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1600x360 --rotate normal --output DP1 --mode 1600x900_60.00 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output VGA1 --off
