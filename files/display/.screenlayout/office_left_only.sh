40339ae277360b069d72e25ed2f7bdd7ccda0d13
Abhay Rana <me@captnemo.in>
2016-10-12 14:30:33 +0530

    [git] Adds a gitconfig alias for fetch and rebase

---
+++ files/display/.screenlayout/office_left_only.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
