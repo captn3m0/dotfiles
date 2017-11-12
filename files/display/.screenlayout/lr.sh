fc21c39aa3ad507dd0b5f0f10eb67484a8a49079
Nemo <me@captnemo.in>
2017-11-12 19:41:26 +0530

    Keep eDP1 disabled while at work

---
+++ files/display/.screenlayout/lr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1 --mode 1600x900 --pos 1920x0 --rotate normal --output eDP1 --off --output VIRTUAL1 --off
