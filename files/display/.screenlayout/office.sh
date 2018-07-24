444f2858a2994b2b31b5c01474bac060c3c28a89
Nemo <me@captnemo.in>
2018-07-24 17:38:12 +0530

    Display changes (changed my seating in office)

--- files/display/.screenlayout/office.sh
+++ files/display/.screenlayout/office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 1600x900 --rotate normal --pos 3840x0
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1440x900 --pos 0x300 --rotate normal --output DP1 --off --output DP1-1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-8 --mode 1920x1200 --pos 1440x0 --rotate normal --output DP2 --off
--- files/display/.screenlayout/single_mac.sh
+++ files/display/.screenlayout/single_mac.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1440x900 --rotate normal --output DP1-1 --off --output DP1 --off --output HDMI2 --off
+xrandr --output eDP1 --mode 1440x900 --rotate normal --output DP1-1 --off --output DP1 --off --output DP1-8 off --output HDMI2 --off
