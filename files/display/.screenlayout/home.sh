b98161f7a80dcd6bf1f49274c0bad31b63a65cc1
Nemo <me@captnemo.in>
2019-01-26 20:21:19 +0530

    butter indentation

--- files/display/.screenlayout/home.sh
+++ files/display/.screenlayout/home.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output HDMI2 --mode 2560x1080 --pos 3840x0 --rotate normal --output HDMI1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off
+xrandr \
+	--output HDMI2 --mode 2560x1080 --pos 1920x0 --rotate normal \
+	--output HDMI1 --off \
+	--output DP1 --mode 1920x1080 --pos 0x0 --rotate normal \
+	--output eDP1 --off \
+	--output VIRTUAL1 --off
