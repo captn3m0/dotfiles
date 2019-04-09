5709348b882a2d7e3cc1e79582a7a3ef10b0edd5
Nemo <me@captnemo.in>
2019-04-10 02:23:42 +0530

    Adds more display modes

---
+++ files/display/.screenlayout/duplicate_mac.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1280x800 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1280x800 --pos 0x0 --rotate normal --output DP2 --off
---
+++ files/display/.screenlayout/recroom.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1360x768 --pos 0x0 --rotate normal --output DP2 --off
